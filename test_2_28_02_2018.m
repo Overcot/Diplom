340a87163c87d7439cbe178915f077a46f6532aa
Overcot <mcivashko@gmail.com>
2018-03-06 18:52:56 +0300

    upd

--- test_2_28_02_2018.m
+++ test_2_28_02_2018.m
@@ -1,7 +1,7 @@
 clear; clc;
-precision = 1;
-L = 3;
-T = 200;
+precision = 0.1;
+L = 51;
+T = 250;
 Ds = precision;
 Dt = precision;
 S_steps= L/Ds;
@@ -15,7 +15,7 @@ mu(s(1:end)) = (s-1)/L*Ds;
 u(s(1:end),t(1:end)) = 0;
 
 for time=t(1:end)
-    u(s(1:end),time) = tand((time-1)*Dt)-(s-1)/L*Ds;
+    u(s(1:end),time) = -1/((time+1)*Dt)-(s-1)/L*Ds;
 end
 
 for time=t(1:end - 1)
@@ -24,3 +24,4 @@ for time=t(1:end - 1)
     x(1,time+1) = trapz(gamma(1:end)'.*x(1:end, time)*Ds);
 end
 
+plot(x(1, t))
