9d10078143ef969a732f27b90e7c23b815b0e757
Overcot <mcivashko@gmail.com>
2018-03-21 01:38:55 +0300

    гзв

--- test_2_28_02_2018.m
+++ test_2_28_02_2018.m
@@ -1,7 +1,7 @@
 clear; clc;
-precision = 0.1;
-L = 51;
-T = 250;
+precision = 1;
+L = 4;
+T = 2;
 Ds = precision;
 Dt = precision;
 S_steps= L/Ds;
@@ -10,18 +10,100 @@ s=1:S_steps+1;
 t=1:T_steps+1;
 gamma(1:S_steps/2) = 0;
 gamma(S_steps/2+1:s(end)) = 2/L;
+gamma(3) = 1/4; % для примера чтобы выровнять нач точку
 x(s(1:end), 1) = 1;
 mu(s(1:end)) = (s-1)/L*Ds;
 u(s(1:end),t(1:end)) = 0;
 
 for time=t(1:end)
-    u(s(1:end),time) = -1/((time+1)*Dt)-(s-1)/L*Ds;
+    u(s(1:end),time) = -1/(time)*Dt - (s-1)/L*Ds;
 end
+%{
+A = -1/(2*Ds);
+B = 1/Dt;
+C = 1/(2*Ds);
+%}
+
+
+
 
 for time=t(1:end - 1)
+    alpha(s) = zeros(size(s,2),1);
+    beta(s) = zeros(size(s,2),1);
+    
+    %считаем самую левую точку из краевого условия в задаче
+    x(1,time+1) = trapz(gamma(1:end)'.*x(1:end, time)*Ds);
+    %считаем самую правую точку с помощью явного левого уголка
+    x(s(end), time + 1) = x(s(end), time) - (Dt/Ds)*(x(s(end), time) - x(s(end)-1, time)) - Dt*(mu(s(end))'.*x(s(end), time) + u(s(end),time).*x(s(end), time));
+    
+    %Неявная схема(метод прогонки)
+    %{
+    for class=s(2:end-1)
+        Fij = -mu(class)*x(class, time)-u(class, time)*x(class, time) + x(class, time)/Dt;
+        if class == s(end)-1
+            beta(class) = (Fij - A*beta(class-1))/(A*alpha(class-1)+B);
+        elseif class == 2
+            alpha(class) = -C/B;
+            beta(class) = Fij/B;
+        else
+            alpha(class) = -C/(A*alpha(class-1)+B);
+            beta(class) = (Fij - A*beta(class-1))/(A*alpha(class-1)+B);
+        end
+    end
+    %}
+    temp = zeros(size(s,2)-2,size(s,2)-2)
+    tempf = zeros(size(s,2)-2,1)
+    for class=s(2:end-1)
+        classx = class-1
+        if class == 2
+            temp(classx, classx) = 1/Dt + mu(class) + u(class, time + 1);
+            temp(classx, classx + 1) = 1/(2*Ds);
+        elseif class == s(end-1)
+            temp(classx, classx-1) = -1/(2*Ds);
+            temp(classx, classx) = 1/Dt + mu(class) + u(class, time + 1);
+        else
+            temp(classx, classx - 1) = -1/(2*Ds);
+            temp(classx, classx) = 1/Dt + mu(class) + u(class, time + 1);
+            temp(classx, classx + 1) = 1/(2*Ds);
+        end
+        tempf(classx) = x(class, time)/Dt;
+    end
+    temp
+    tempf
+    linsolve(temp, tempf)
+    for class=s(2:end-1)
+        A = -1/(2*Ds);
+        B = 1/Dt + mu(class) + u(class, time + 1);
+        C = 1/(2*Ds);
+        Fij = x(class, time)/Dt;
+        if class == s(end)-1
+            beta(class) = (Fij - A*beta(class-1))/(A*alpha(class-1)+B);
+        elseif class == 2
+            alpha(class) = -C/B;
+            beta(class) = Fij/B;
+        else
+            alpha(class) = -C/(A*alpha(class-1)+B);
+            beta(class) = (Fij - A*beta(class-1))/(A*alpha(class-1)+B);
+        end
+    end
+    
+
+
+    x(s(end)-1, time + 1) = beta(s(end)-1);
+    for class=s(end-2:-1:2)
+        x(class, time + 1) = beta(class) + alpha(class) * x(class + 1, time + 1);
+    end
+
+    
+    %{
     x(s(2:end-1), time + 1) = x(s(2:end-1), time) - (Dt/(2*Ds))*(x(s(2:end-1)+1, time) - x(s(2:end-1)-1, time)) - Dt*(mu(s(2:end-1))'.*x(s(2:end-1), time) + u(s(2:end-1),time).*x(s(2:end-1), time));
     x(s(end), time + 1) = x(s(end), time) - (Dt/Ds)*(x(s(end), time) - x(s(end)-1, time)) - Dt*(mu(s(end))'.*x(s(end), time) + u(s(end),time).*x(s(end), time));
-    x(1,time+1) = trapz(gamma(1:end)'.*x(1:end, time)*Ds);
+    %}
+    
+    
+    
+    
+    
+    
 end
-
 plot(x(1, t))
