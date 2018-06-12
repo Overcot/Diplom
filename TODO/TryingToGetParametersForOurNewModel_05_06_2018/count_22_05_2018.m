044445ee1c3d58d8f40b25674f219d9bfdaf5dfb
Overcot <mcivashko@gmail.com>
2018-06-12 15:36:08 +0300

    upd

--- TODO/TryingToGetParametersForOurNewModel_05_06_2018/count_22_05_2018.m
+++ TODO/TryingToGetParametersForOurNewModel_05_06_2018/count_22_05_2018.m
@@ -1,10 +1,10 @@
 clear; clc;
 global recruitment ssb
 
-recruitment = xlsread('SRdata_ICES','D:D')
-ssb = xlsread('SRdata_ICES','C:C')
+recruitment = xlsread('SRdata_ICES','D3:D50')
+ssb = xlsread('SRdata_ICES','C2:C49')
 
-init = [1, 1];
+init = [3416, 5*10^(-8)];
 
 options = optimset('Display','iter','PlotFcns',@optimplotfval,'Tolx',1e-20,'TolFun',1e-20);
 [x, val, exitflag,output] = fminsearch(@recruitment_func, init,options)
--- TODO/TryingToGetParametersForOurNewModel_05_06_2018/recruitment_func.m
+++ TODO/TryingToGetParametersForOurNewModel_05_06_2018/recruitment_func.m
@@ -1,6 +1,6 @@
 function f = recruitment_func(ab)
 global recruitment ssb
 f = 0;
-for t = 1:53
+for t = 1:length(recruitment)
     f = f + (log(recruitment(t)) - log(ab(1)*ssb(t)) - (-ab(2).*ssb(t)) ).^2;
 end
