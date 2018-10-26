481c9a4519ab42f4eca15f1f58ffdc721257ec2e
Alexey Ivashko <mcivashko@gmail.com>
2018-10-26 08:21:22 +0300

    upd for long counting

---
+++ TODO/TryingToGetParametersForOurNewModel_05_06_2018/countParameters_26_10_2018.m
@@ -0,0 +1,56 @@
+clear; clc;
+global recruitment ssb maxSSB lowerSSB higherSSB
+
+year = 2011 %possible values: 2011, 2010
+model = 'Anna' %possible values: 'Anna', 'Ricker'
+
+%% Import Data
+if (year == 2010)
+    recruitment = xlsread('ourData2010&2011','D3:D49');
+    ssb = xlsread('ourData2010&2011','B4:B50');
+    maxSSB = max(ssb);
+elseif (year == 2011)
+    recruitment = xlsread('ourData2010&2011','H3:H50');
+    ssb = xlsread('ourData2010&2011','F4:F51');
+    maxSSB = max(ssb);
+end
+
+%% Try finding parameters using fmincon
+% Init data
+xdata = ssb;
+ydata = log(recruitment);
+optionslsqnonlin = optimoptions(@lsqnonlin, 'Algorithm','trust-region-reflective', 'Display', 'iter', 'Tolx',1e-20,'TolFun',1e-20);
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+for leftAborder=-10000000:0.1:0
+    for leftBborder = leftAborder:0.1:0
+        for rightABorder=0:0.1:100000000
+            for rightBborder=0:0.1:10000000
+                for leftAllee=0:0.01:1
+                    for rightAllee = leftAllee:0.01:1
+                        if (model == 'Anna')
+                            init = [8,0,0.14];
+                            lowerSSB = 0;
+                            higherSSB = 0;
+                            [x, resnorm] = fmincon(@AnnaModel, init, A,b,Aeq,beq,[-10000000,-10000000,0], [100000000,10000000,0.25]);
+
+                        end
+                        fileName = 'possibleVariants.xlsx';
+                        fileExist = exist(fileName,'file'); 
+                        if fileExist==0
+                            header = {'A','B','Allee','resnorm', 'leftA','leftB','leftAllee', 'rightA', 'rightB', 'rightAllee'};
+                            xlswrite(fileName,header);
+                        end
+                        [~,~,input] = xlsread(fileName); % Read in your xls file to a cell array (input)
+                        new_data = {x(1), x(2), x(3), resnorm, leftAborder, leftBborder, leftAllee, rightABorder, rightBborder, rightAllee}; % This is a cell array of the new line you want to add
+                        output = cat(1,input,new_data); % Concatinate your new data to the bottom of input
+                        xlswrite(fileName,output); % Write to the new excel file. 
+                    end
+                end
+            end
+        end
+    end
+end
+
--- TODO/TryingToGetParametersForOurNewModel_05_06_2018/count_22_05_2018.m
+++ TODO/TryingToGetParametersForOurNewModel_05_06_2018/count_22_05_2018.m
@@ -57,6 +57,22 @@ x
 resnorm
 lowerSSB
 higherSSB
+fileName = 'possibleVariants.xlsx'
+N='Adnan'; a=22; roll=22; gpa=3.55;
+fileExist = exist(fileName,'file'); 
+if fileExist==0
+    header = {'Name', 'age ','roll' , 'gpa'};
+    xlswrite(filename,header);
+
+
+else
+
+    [~,~,input] = xlsread(filename); % Read in your xls file to a cell array (input)
+    new_data = {N, a,roll , gpa}; % This is a cell array of the new line you want to add
+    output = cat(1,input,new_data); % Concatinate your new data to the bottom of input
+    xlswrite(filename,output); % Write to the new excel file. 
+
+end
 %% Count AIC
 %resnorm = 11.44
 %n = size(recruitment, 1)
